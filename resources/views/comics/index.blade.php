77a4a219beefe571a1f0c262cefa8140d1d9f04f
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-09-16 15:28:59 +0200

    style: adjust img and col

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,35 +5,38 @@
 
 @section('content')
 <div class="container my-5">
-    <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">id</th>
-            <th scope="col">Immagine</th>
-            <th scope="col">Nome fumetto</th>
-            <th scope="col">Prezzo</th>
-            <th scope="col">Serie</th>
-            <th scope="col">Tipo</th>
-            <th scope="col">Data</th>
-            <th scope="col">Descrizione</th>
-          </tr>
-        </thead>
-        <tbody>
-            @foreach ( $comics as $comic )
-                <tr>
-                        <td>{{$comic->id}}</td>
-                        <td> <img src="{{$comic->thumb}}" alt="{{$comic->title}}"></td>
-                        <td>{{$comic->title}}</td>
-                        <td>{{$comic->price}}</td>
-                        <td>{{$comic->series}}</td>
-                        <td>{{$comic->type}}</td>
-                        <td>{{($comic->sale_date)->format('d-m-Y')}}</td>
-                        <td>{{$comic->description}}</td>
-                </tr>
-            @endforeach
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">id</th>
+                <th scope="col">Immagine</th>
+                <th scope="col">Nome fumetto</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Serie</th>
+                <th scope="col">Tipo</th>
+                <th scope="col">Data</th>
+                <th scope="col">Descrizione</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ( $comics as $comic )
+                    <tr>
+                            <td class="col-auto">{{$comic->id}}</td>
+                            <td class="col-auto"> <img class="icon" src="{{$comic->thumb}}" alt="{{$comic->title}}"></td>
+                            <td class="col-auto" class="col-auto">{{$comic->title}}</td>
+                            <td class="col-auto">{{$comic->price}}</td>
+                            <td class="col-auto">{{$comic->series}}</td>
+                            <td class="col-auto">{{$comic->type}}</td>
+                            <td class="col-2">{{($comic->sale_date)->format('d-m-Y')}}</td>
+                            <td class="col-2">{{$comic->description}}</td>
+                    </tr>
+                @endforeach
 
-        </tbody>
-      </table>
+            </tbody>
+          </table>
+    </div>
+
 </div>
 
 @endsection
