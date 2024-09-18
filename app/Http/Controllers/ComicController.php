897e280d1f4b6c55525d51f20ea3f487ca99f968
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-09-18 13:32:59 +0200

    feat: add second parameter in validate to modify the alert

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -42,6 +42,23 @@ class ComicController extends Controller
             'series'=>'required|min:3|max:70',
             'sale_date'=>'required|date',
             'type'=>'required|min:3|max:70',
+        ], [
+            'title.required' => 'Il titolo è un campo obbligatorio',
+            'title.min' => 'Il titolo deve avere almeno :min caratteri',
+            'thumb.required' => 'L\'immagine è un campo obbligatorio',
+            'thumb.min' => 'L\'immagine deve avere almeno :min caratteri',
+            'price.required' => 'Il prezzo è un campo obbligatorio',
+            'price.min' => 'Il prezzo deve avere almeno :min caratteri',
+            'price.max' => 'Il prezzo deve avere massimo :max caratteri',
+            'series.required' => 'La serie è un campo obbligatorio',
+            'series.min' => 'La serie deve avere almeno :min caratteri',
+            'series.max' => 'La serie deve avere massimo :max caratteri',
+            'sale_date.required' => 'La data è un campo obbligatorio',
+            'sale_date.date' => 'La data deve avere un formato corretto',
+            'type.required' => 'La tipologia è un campo obbligatorio',
+            'type.min' => 'La tipologia deve avere almeno :min caratteri',
+            'type.max' => 'La tipologia deve avere massimo :max caratteri',
+
         ]);
 
         //questo prende i dati del form e lo salva nel DB
